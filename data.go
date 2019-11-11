680d794ac84544ad918abbdc289d7d6a615532ba
Eric <ericwongmoon@outlook.com>
2019-11-11 22:11:12 +0800

    Initial commit

---
+++ data.go
@@ -0,0 +1,65 @@
+package SimulatedAnnealing
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+var DataFileName = "1.att48.tsp.txt"
+
+type Data struct {
+	CityNum int
+	PosX float64
+	PosY float64
+}
+
+func (d Data) String() string {
+	return fmt.Sprintf("[CityNum:%3d, PosX:%5f, PosY:%5f]\n", d.CityNum,d.PosX,d.PosY)
+}
+
+var data []Data
+
+func ReadData() []Data {
+	return data
+}
+
+func InitSampleData() {
+	data = make([]Data, 0)
+	var d Data
+	file, _ := GetFileLines()
+	for _, s := range file {
+		sp := strings.Split(s, " ")
+		sp0, _ := strconv.Atoi(sp[0])
+		sp1, _ := strconv.Atoi(sp[1])
+		sp2, _ := strconv.Atoi(sp[2])
+		d = Data{sp0, float64(sp1), float64(sp2)}
+		data = append(data, d)
+	}
+}
+
+
+func GetFileLines() ([]string, error) {
+	var result []string
+	filePath := "./data/" + DataFileName
+	b, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Printf("read file: %v error: %v", filePath, err)
+		return result, err
+	}
+	s := string(b)
+	for _, lineStr := range strings.Split(s, "\n") {
+		lineStr = strings.TrimSpace(lineStr)
+		if lineStr == "" {
+			continue
+		}
+		result = append(result, lineStr)
+	}
+	return result[6:len(result) - 1], nil
+}
+
+func GetDataNum() int {
+	data := ReadData()
+	return len(data)
+}
---
+++ sa.go
@@ -0,0 +1,8 @@
+package SimulatedAnnealing
+
+var Temp = 100 //初始化温度
+var TMin = 0.0000001 //温度下界限
+var K = 100 //迭代次数
+var delta = 0.98 //温度下降率
+
+
